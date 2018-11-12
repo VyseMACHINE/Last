095b6c7782a1e9125b60f836cd8f5e6d7c7a09fd
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2018-11-12 21:50:53 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Item
+    {
+        public  string Title { get; set; } = ConfigurationSettings.AppSettings["pathXmlItem"].ToString();
+        public  string Link { get; set; } = ConfigurationSettings.AppSettings["pathXmlItem"].ToString();
+        public  string Description { get; set; } = ConfigurationSettings.AppSettings["pathXmlItem"].ToString();
+        public DateTime PupDate { get; set; } 
+
+        public static Item GetManDetails(string title, string Link, string Description)
+        {
+            Item item = new Item(); //потому что возвращаем тип Man
+
+            XmlDataDocument xmlDoc = new XmlDataDocument();
+            xmlDoc.Load(Title);
+            XmlNode root = xmlDoc.DocumentElement;
+            XmlNode node = root.SelectSingleNode(string.Format("Item[Title='{0}' Link='{1}' Description='{2}' ]", Title, Link, Description));
+
+            XmlNodeList list = node.ChildNodes;
+
+            for (int i = 0; i < list.Count; i++)
+                item.[list.Item(i).Name] = list.Item(i).InnerText;
+
+            return item;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Item> item = new List<Item>
+            {
+            new Item
+                {
+               Title = "XML",
+               Link = "https://habr.com/rss/interesting/",
+               Description = "This XML file does not appear to have any style information associated with it. The document tree is shown below",
+               PupDate = DateTime.Now
+                }
+            };           
+            Console.ReadKey();
+        }
+    }
+}
